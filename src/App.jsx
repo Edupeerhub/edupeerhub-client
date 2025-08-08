4ad2e02c232cd1291b049440e5d9591f4ce6ac52
Taofeeqoh24 <taofeeqohrabbit20@gmail.com>
2025-08-08 14:54:13 +0100

    feat:project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className='bg-primary text-white font-roboto p-4 h-full justify-center align-center text-center'>EdupeerHub</div>
+
+    </>
+  )
+}
+
+export default App
